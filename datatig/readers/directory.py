b60fa7718685076d126a8bff18c3abcb3e0106ad
James <james.baster@opendataservices.coop>
2020-04-28 17:21:57 +0100

    reader: Only load files with .json extension

--- datatig/readers/directory.py
+++ datatig/readers/directory.py
@@ -11,8 +11,9 @@ def process_type(config, type, datastore):
         full_sourcedir = os.path.join(full_sourcedir, config.git_submodule_directory())
     for path, subdirs, files in os.walk(start_dir):
         for name in files:
-            full_filename = os.path.abspath(os.path.join(path, name))
-            process_file(config, type, full_filename,  full_filename[len(full_sourcedir)+1:], name[:-5], datastore)
+            if name.endswith('.json'):
+                full_filename = os.path.abspath(os.path.join(path, name))
+                process_file(config, type, full_filename,  full_filename[len(full_sourcedir)+1:], name[:-5], datastore)
 
 
 def process_file(config, type, filename_absolute, filename_relative_to_git, id, datastore):
