f2a0f73ac38f02b992edf76226f7be54aef1fcc9
James <james.baster@opendataservices.coop>
2020-04-29 12:46:10 +0100

    validatejsonschema: Show all errors, not just one

--- datatig/validate/jsonschema.py
+++ datatig/validate/jsonschema.py
@@ -18,18 +18,19 @@ class JsonSchemaValidator:
 
         with open(os.path.join(self.config.source_dir, type_config.json_schema())) as fp:
             schema = json.load(fp)
+        # TODO use correct version of Draft Validator
+        schema_validator = jsonschema.Draft3Validator(schema)
 
         for item_id in self.datastore.get_ids_in_type(type_id):
             data_item = self.datastore.get_item(type_id, item_id)
 
-            try:
-                jsonschema.validate(instance=data_item.data, schema=schema)
-                self.datastore.store_json_schema_validation_pass(type_id, item_id)
-            except Exception as err:
-                err_data = {
+            errors = sorted(schema_validator.iter_errors(data_item.data), key=str)
+            if errors:
+                err_data = [{
                     'message': err.message,
                     'path': err.path,
                     'schema_path': err.schema_path,
-                }
-                self.datastore.store_json_schema_validation_errors(type_id, item_id, [ err_data ])
-
+                } for err in errors]
+                self.datastore.store_json_schema_validation_errors(type_id, item_id, err_data)
+            else:
+                self.datastore.store_json_schema_validation_pass(type_id, item_id)
