80d38b577e134266df39892c1fd67439ea6787fb
James <james.baster@opendataservices.coop>
2020-05-06 17:13:14 +0100

    setup & readme & license: Add content

--- readme.md
+++ readme.md
@@ -1,9 +1,6 @@
 # DataTig
 
+Source: https://github.com/DataTig/DataTig
+Issues: https://github.com/DataTig/DataTig/issues
+Documentation: https://datatig.readthedocs.io/en/latest/
 
-
-
-## Dev
-
-
-    python -m http.server 
--- setup.py
+++ setup.py
@@ -4,6 +4,16 @@ import setuptools
 setuptools.setup(
     name="DataTig", 
     version="0.0.1",
+    description="A tool for working with crowd sourced data in JSON files in a Git repository",
+    url="https://github.com/DataTig/DataTig",
+    project_urls={
+        'Documentation': 'https://datatig.readthedocs.io/en/latest/',
+        'Issues': 'https://github.com/DataTig/DataTig/issues',
+        'Source': 'https://github.com/DataTig/DataTig',
+    },
+    author="Open Data Services",
+    author_email="code@opendataservices.coop",
+    license="MIT",
     packages=setuptools.find_packages(exclude=['test']),
     package_data={
         "": ["*.html", "*.txt", "*.css","*.js"],
