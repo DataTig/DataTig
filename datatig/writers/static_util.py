7b1806840816359f6ddf6fd8db6b3dbdd235924c
James <james.baster@opendataservices.coop>
2022-02-08 15:09:02 +0000

    black: New version of black is out

--- datatig/writers/static_util.py
+++ datatig/writers/static_util.py
@@ -11,8 +11,8 @@ _js_escapes = {
     "=": "\\u003D",
     "-": "\\u002D",
     ";": "\\u003B",
-    u"\u2028": "\\u2028",
-    u"\u2029": "\\u2029",
+    "\u2028": "\\u2028",
+    "\u2029": "\\u2029",
 }
 # Escape every ASCII character with a value less than 32.
 _js_escapes.update(("%c" % z, "\\u%04X" % z) for z in range(32))
